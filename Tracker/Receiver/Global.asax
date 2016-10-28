7a175ca76844a52eb5a0c8f39a12698c1f78a290
paulhuangbest <good_hy@163.com>
2016-10-28 17:28:31 +0800

    Empty

---
+++ Tracker/Receiver/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Receiver.MvcApplication" Language="C#" %>
---
+++ Tracker/Receiver/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Receiver.Startup))]
+namespace Receiver
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
