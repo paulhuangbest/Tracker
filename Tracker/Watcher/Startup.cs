9129583756f18a25dfb9806741347c5c75ffcb37
paulhuangbest <good_hy@163.com>
2016-12-06 19:14:01 +0800

    add watcher

---
+++ Tracker/Watcher/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Watcher.Startup))]
+namespace Watcher
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
