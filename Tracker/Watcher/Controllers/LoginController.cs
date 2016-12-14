aa6dfaef330be5f8df6a81c934e6fc900cb36e59
paulhuangbest <good_hy@163.com>
2016-12-14 22:38:05 +0800

    add authorization

---
+++ Tracker/Watcher/Controllers/LoginController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNet.Identity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Watcher.Controllers
+{
+    public class LoginController : Controller
+    {
+        //
+        // GET: /Login/
+        public ActionResult Index()
+        {
+            return View("Login");
+        }
+
+        public ActionResult Register()
+        {
+            var claims = new List<Claim>();
+            //claims.Add(new Claim(ClaimTypes.Name,"Paul"));
+            claims.Add(new Claim("WatcherUser", "Paul"));
+            claims.Add(new Claim("Email", "good_hy@163.com"));
+            var id = new ClaimsIdentity(claims,DefaultAuthenticationTypes.ApplicationCookie);
+
+            var ctx = Request.GetOwinContext();
+            var authenticationManager = ctx.Authentication;
+            authenticationManager.SignIn(id);
+
+            return RedirectToAction("Index", "Home");
+        }
+	}
+}
